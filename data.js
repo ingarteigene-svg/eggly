8493b1d805d0d84e42a753532ba3a44ef6c9da07
ingarteigene-svg <ingarteigene@icloud.com>
2026-07-07 16:50:45 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,138 @@
+window.EGGLY_DATA = {
+ "oppdatert": "07.07.2026",
+ "place": "Teigene Egg · Hareid",
+ "egg": {
+  "sisteDatoLang": "24. jun 2026",
+  "dev": [
+   {
+    "dato": "05.06",
+    "vekt": 43.15,
+    "pris": 0.7934,
+    "mal": null
+   },
+   {
+    "dato": "10.06",
+    "vekt": 45.66,
+    "pris": 0.953,
+    "mal": 46.31
+   },
+   {
+    "dato": "17.06",
+    "vekt": 49.66,
+    "pris": 1.2552,
+    "mal": 46.22
+   },
+   {
+    "dato": "24.06",
+    "vekt": 52.22,
+    "pris": 1.4171,
+    "mal": 46.08
+   }
+  ],
+  "siste": {
+   "antall": 86400,
+   "brutto": 122441,
+   "prisEgg": 1.42,
+   "kostbasis": 1.54,
+   "mlVekt": 56.4,
+   "brettUke": 1259,
+   "malBrett": 46.08,
+   "vekt": 52.2
+  },
+  "pallTotalt": 10.8,
+  "totalt": {
+   "egg": 259200,
+   "brutto": 314767,
+   "brett": 2157,
+   "pall": 10.8
+  },
+  "miks": [
+   [
+    "XL",
+    0.5,
+    "#00643c"
+   ],
+   [
+    "L",
+    1.5,
+    "#1A7A52"
+   ],
+   [
+    "M",
+    42.2,
+    "#4A9B73"
+   ],
+   [
+    "S",
+    51.1,
+    "#82B89C"
+   ],
+   [
+    "SI",
+    3.6,
+    "#f5a623"
+   ],
+   [
+    "Øvrig",
+    1.2,
+    "#9AA0A6"
+   ]
+  ]
+ },
+ "for": {
+  "sisteDatoLang": "1. jul 2026",
+  "dev": [
+   {
+    "dato": "18.05",
+    "krKg": 5.602,
+    "kvantum": 12000
+   },
+   {
+    "dato": "27.05",
+    "krKg": 5.64,
+    "kvantum": 12000
+   },
+   {
+    "dato": "03.06",
+    "krKg": 5.64,
+    "kvantum": 12000
+   },
+   {
+    "dato": "08.06",
+    "krKg": 5.64,
+    "kvantum": 10000
+   },
+   {
+    "dato": "17.06",
+    "krKg": 5.64,
+    "kvantum": 13000
+   },
+   {
+    "dato": "24.06",
+    "krKg": 5.696,
+    "kvantum": 12500
+   },
+   {
+    "dato": "01.07",
+    "krKg": 5.696,
+    "kvantum": 13000
+   }
+  ],
+  "siste": {
+   "kvantum": 13000,
+   "netto": 74048,
+   "krKg": 5.696,
+   "faktura": 92559,
+   "mva": 18512,
+   "grunnpris": 5.83,
+   "frakt": 6006,
+   "rabatter": -7748
+  },
+  "totalt": {
+   "kvantum": 84500,
+   "netto": 477528,
+   "krKg": 5.651,
+   "leveranser": 7
+  }
+ }
+};
